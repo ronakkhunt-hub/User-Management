5f31184c03986bbe64cb5480cd4d30e520601caf
Bhautik Patel <bhautikpatel8494@gmail.com>
2021-11-18 16:24:50 +0530

    shopify web changes

--- src/utils/axiosApi.js
+++ src/utils/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3002/"
+    baseURL: process.env.REACT_APP_PRODUCTMANAGEURL
 });
 
 let token = localStorage.getItem('loggedIn')
